be2e5893d32f79cc6d0f10886803c9c078a05513
unknown <3315131925@qq.com>
2024-04-19 19:30:30 +0800

    Init project

---
+++ miniprogram-foodmap-master/miniprogram-foodmap-master/miniprogram/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "appName":"迪迪搭搭",
+  "envID":"cloud1-5go2hx89296fd52a",
+  "mapSubKey":"EAOBZ-MNUK5-FBQIE-I7HM5-QB4BH-SKBGB",
+  "center_longitude": 113.921736,
+  "center_latitude": 22.538017,
+  "dynamic_title":true,
+  "show_admin":true,
+  "default_scale":16
+}
